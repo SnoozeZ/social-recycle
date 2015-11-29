9df2cd281f8f8133304e139448d4a1b1949b21f5
billjyc <jiayicong@vip.qq.com>
2015-11-28 21:21:04 -0800

    join between category and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,6 +29,7 @@ class Item < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100}
   validates :description, presence: true, length: {maximum: 1024}
+  validates :category, presence: true
 
   #ordering the item with default_scope
   default_scope -> { order(created_at: :desc)}
