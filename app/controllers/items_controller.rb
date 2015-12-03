afba632d04c765027f7b50ea902aa2303a268b73
LeagueOfCodes <returnname@gmail.com>
2015-12-02 18:21:10 -0800

    ByCategory

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,11 +18,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  def index type
-    if type == 0
+  def index
+    if cat_params[:category] == "0"
       @items = Item.where(is_valid: true, status: 0).paginate(page: params[:page])
     else
-      @items = Item.where(is_valid: true, status: 0, category_id: type).paginate(page: params[:page])
+      @items = Item.where(is_valid: true, status: 0, category_id: cat_params[:category].to_i).paginate(page: params[:page])
     end
   end
 
@@ -36,6 +36,9 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:title, :description, :timeout,
                                    :avatar, :category_id, :avatar_cache, :status)
+    end
+    def cat_params
+      params.require(:type).permit(:category)
     end
 
 end
