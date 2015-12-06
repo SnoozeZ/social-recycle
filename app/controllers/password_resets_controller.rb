3ed122d0a3f2b58cf8a3d2f7a0b6925fd23a0321
ToumaKazusa <gaofc410@gmail.com>
2015-12-06 12:09:10 -0800

    redirect to signin after reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
+      redirect_to signin_url
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
