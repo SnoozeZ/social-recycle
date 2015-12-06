d0ed42de21667f0fc6caa147e681227cd29d9bf5
GrassSunFlower <cpf11@mails.tsinghua.edu.cn>
2015-12-06 12:55:31 -0800

    Add range search

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,7 +5,12 @@ class MapController < ApplicationController
     if(params[:distance])
       distance = params[:distance]
       puts "searching by distance"
-      @postions = Item.where(is_valid: true, status: 0).search(params[:search]).paginate(page: params[:page])
+      puts params[:lat]
+      puts params[:lng]
+      lat = params[:lat][:lat].to_f
+      lng = params[:lng][:lat].to_f
+      @postions = Item.find(:all, :conditions => ["lat between ? and ?", lat, lat-1, "lng between ? and ?", lng, lng-1])
+      # @postions = Item.where(is_valid: true, status: 0, lat: params[:lat], lng: params[:lng])
     else
       params[:distance] = 1.0
     end
