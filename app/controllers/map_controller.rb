1c92dda1908f13717292a1601557ce9c015b7535
GrassSunFlower <cpf11@mails.tsinghua.edu.cn>
2015-12-06 10:56:04 -0800

    tmp save2

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,7 +4,8 @@ class MapController < ApplicationController
   def mapsearch
     if(params[:distance])
       distance = params[:distance]
-
+      puts "searching by distance"
+      @postions = Item.where(is_valid: true, status: 0).search(params[:search]).paginate(page: params[:page])
     else
       params[:distance] = 1.0
     end
