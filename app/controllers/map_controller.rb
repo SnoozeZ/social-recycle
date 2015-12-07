cf8132ea6b0de4477decf83651a24aff90b71687
GrassSunFlower <cpf11@mails.tsinghua.edu.cn>
2015-12-06 20:33:11 -0800

    range find

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,22 +2,23 @@ class MapController < ApplicationController
 
 
   def mapsearch
-    if(params[:distance])
-      distance = params[:distance]
+    if(params[:distance] && params[:lat][:lat] && params[:lng][:lng])
+      distance = params[:distance].to_f
       puts "searching by distance"
       puts params[:lat]
       puts params[:lng]
       lat = params[:lat][:lat].to_f
-      lng = params[:lng][:lat].to_f
+      lng = params[:lng][:lng].to_f
       ulat = lat + distance/85.0
       llat = lat - distance/85.0
       ulng = lng + distance/111.7
       llng = lng - distance/111.7
+      puts ulat, llat, ulng, llng
       # @positions = Item.find(:all, :conditions => ["lat between ? and ?", -180, 180])
-      @positions = Item.where("lat between ? and ?", llat, ulat,"lng between ? and ?", llng, ulng)
+      @positions = Item.where("lat between ? and ? and lng between ? and ?", llat, ulat, llng, ulng)
     else
       params[:distance] = 1.0
-      @positions = Item.where("lat between ? and ?", llat, ulat,"lng between ? and ?", llng, ulng)
+      @positions = nil
     end
 
   end
