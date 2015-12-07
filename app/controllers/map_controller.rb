6ae162e0880fdfd10990a609ab8c21eaa43b5721
GrassSunFlower <cpf11@mails.tsinghua.edu.cn>
2015-12-06 19:20:23 -0800

    tmp save

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -9,11 +9,15 @@ class MapController < ApplicationController
       puts params[:lng]
       lat = params[:lat][:lat].to_f
       lng = params[:lng][:lat].to_f
+      ulat = lat + distance/85.0
+      llat = lat - distance/85.0
+      ulng = lng + distance/111.7
+      llng = lng - distance/111.7
       # @positions = Item.find(:all, :conditions => ["lat between ? and ?", -180, 180])
-      @positions = Item.where("lat between ? and ?", -180, 180)
+      @positions = Item.where("lat between ? and ?", llat, ulat,"lng between ? and ?", llng, ulng)
     else
       params[:distance] = 1.0
-      @positions = Item.where("lat between ? and ?", -180, 180)
+      @positions = Item.where("lat between ? and ?", llat, ulat,"lng between ? and ?", llng, ulng)
     end
 
   end
